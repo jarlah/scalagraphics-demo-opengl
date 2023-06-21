8eec6c7b2f03ee4227410c9ef704b9915b5f56eb
Jarl André Hübenthal <jarlah@protonmail.com>
2023-06-21 14:20:35 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,29 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.3.0"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "scalagraphics-demo-opengl",
+    organization := "com.github.jarlah.scalagraphics",
+    idePackagePrefix := Some("com.github.jarlah.scalagraphics"),
+    libraryDependencies ++= {
+      val version = "3.3.1"
+      val os = "linux" // TODO: Change to "linux" or "macos" if necessary
+      Seq(
+        "lwjgl",
+        "lwjgl-glfw",
+        "lwjgl-opengl"
+        // TODO: Add more modules here
+      ).flatMap { module =>
+        {
+          Seq(
+            "org.lwjgl" % module % version,
+            "org.lwjgl" % module % version % "runtime" classifier s"natives-$os"
+          )
+        }
+      }
+    },
+    libraryDependencies += "com.github.jarlah.scalagraphics" % "scalagraphics_3" % "0.3.0-SNAPSHOT",
+    libraryDependencies += "org.joml" % "joml" % "1.10.5"
+  )
---
+++ src/main/scala/main.scala
@@ -0,0 +1,64 @@
+package com.github.jarlah.scalagraphics
+
+import org.lwjgl.glfw.Callbacks.glfwFreeCallbacks
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.*
+
+@main
+def main(): Unit = {
+  if (!glfwInit())
+    throw new IllegalStateException("Unable to initialize GLFW")
+
+  glfwDefaultWindowHints()
+  glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+
+  val window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
+  if (window == NULL)
+    throw new RuntimeException("Failed to create the GLFW window")
+
+  glfwSetKeyCallback(window, (window, key, scancode, action, mods) => {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+      glfwSetWindowShouldClose(window, true)
+  })
+
+  val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+  glfwSetWindowPos(window, (vidmode.width() - 300) / 2, (vidmode.height() - 300) / 2)
+
+  glfwMakeContextCurrent(window)
+  glfwSwapInterval(1)
+  glfwShowWindow(window)
+
+  GL.createCapabilities()
+
+  val graphics = new OpenGLGraphicsIO()
+  graphics.setupShaderProgram()
+  graphics.setupRectangle()
+  graphics.setWindowSize(300, 300)
+
+  glfwSetWindowSizeCallback(window, (window, width, height) => {
+    GL11.glViewport(0, 0, width, height)
+    graphics.setWindowSize(width, height)
+  })
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    drawGreenRectangle(30, 100, 100, 100).run(graphics)
+      .getOrElse(throw new RuntimeException("Failed to set color"))
+
+    glfwSwapBuffers(window)
+    glfwPollEvents()
+  }
+
+  glfwFreeCallbacks(window)
+  glfwDestroyWindow(window)
+  glfwTerminate()
+}
+
+def drawGreenRectangle(x: Int, y: Int, width: Int, height: Int): GraphicsOp[Unit] =
+  GraphicsOp.setColor(GraphicsIO.Green)
+    >> GraphicsOp.drawRect(x, y, width, height)
